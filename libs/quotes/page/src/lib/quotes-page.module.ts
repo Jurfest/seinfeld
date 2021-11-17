82e3b592e687f89be14756ec146946ca6e00568e
Jurfest <jurfest.apps@gmail.com>
2021-11-17 09:38:00 -0300

    chore: generate component for quotes page feature

--- libs/quotes/page/src/lib/quotes-page.module.ts
+++ libs/quotes/page/src/lib/quotes-page.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuotesPageComponent } from './quotes-page/quotes-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    QuotesPageComponent
+  ],
 })
 export class QuotesPageModule {}
---
+++ libs/quotes/page/src/lib/quotes-page/quotes-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'seinfeld-quotes-page',
+  templateUrl: './quotes-page.component.html',
+  styleUrls: ['./quotes-page.component.scss']
+})
+export class QuotesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
